aab78850d800ff27ceb69b759e69eb50a510fb92
harneizel <159352309+harneizel@users.noreply.github.com>
2026-04-10 13:56:52 +0300

    10.04.26

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp3
+{
+    public class Example
+    {
+        private string _word;
+        private int _number;
+        private bool _isValid;
+
+        public Example(string word = "Unknown", int number = -1, bool isValid = true, char color = '0')
+        {
+            _word = word;
+            _number = number;
+            _isValid = isValid;
+        }
+    }   
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Example e1 = new Example("aaa", 111, true);
+            Example e2 = new Example("aaa", 111);
+            Example e3 = new Example("aaa");
+        }
+    }
+
+
+}   
